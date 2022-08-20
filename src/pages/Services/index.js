839fd8b4bf0f0e21b23fde9acc60f746dae1b5ec
Klicer <51763508+Klicer@users.noreply.github.com>
2022-08-20 22:00:58 +0300

    Add skeleton loading to /services

--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Stats from "api/Stats";
+import { Skeleton } from "components/Skeleton";
 
 const Services = () => {
-  const [services, setServices] = React.useState([]);
+  const [services, setServices] = React.useState(null);
 
   const refreshServices = async () => {
     try {
@@ -15,6 +16,7 @@ const Services = () => {
       console.log(services);
     } catch (e) {
       // TODO: HANDLE ERROR
+      setServices([]);
     }
   };
 
@@ -23,8 +25,8 @@ const Services = () => {
   }, []);
 
   return (
-    <div className="bg-dark p-2 rounded rounded-l-none h-5/6 overflow-y-scroll border-banano-yellow border-l-4">
-      <table className="text-gray-300 ">
+    <div className="bg-dark p-2 rounded rounded-l-none w-full max-w-3xl h-5/6 overflow-y-scroll border-banano-yellow border-l-4">
+      <table className="text-gray-300 w-full">
         <thead>
           <tr className="[&>*]:font-medium table-fixed [&>*]:py-2 border-b-2 border-banano-yellow/25">
             <th>Name</th>
@@ -33,17 +35,39 @@ const Services = () => {
           </tr>
         </thead>
         <tbody>
-          {services.map((service) => {
-            return (
-              <tr className="[&>*]:py-1 border-b-1 border-banano-yellow/25 [&>*]:px-1 [&>*]:md:px-12">
-                <td>{service.name}</td>
-                <td>
-                  <a href={service.website}>{service.website}</a>
-                </td>
-                <td className="text-right">{service.amountOfRequests}</td>
-              </tr>
-            );
-          })}
+          {services ? (
+            <React.Fragment>
+              {services.map((service) => {
+                return (
+                  <tr className="[&>*]:first:pt-4 [&>*]:py-1 border-b-1 border-banano-yellow/25 [&>*]:px-1 [&>*]:sm:px-4 [&>*]:md:px-12">
+                    <td>{service.name}</td>
+                    <td>
+                      <a href={service.website}>{service.website}</a>
+                    </td>
+                    <td className="text-right">{service.amountOfRequests}</td>
+                  </tr>
+                );
+              })}
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              {new Array(30).fill({}).map((_, index) => {
+                return (
+                  <tr className="[&>*]:first:pt-4 [&>*]:py-1 border-b-1 border-banano-yellow/25 [&>*]:px-1 [&>*]:md:px-12">
+                    <td>
+                      <Skeleton text="Dummy Service" />
+                    </td>
+                    <td>
+                      <Skeleton text="https://dummy.service" />
+                    </td>
+                    <td className="flex justify-end">
+                      <Skeleton text="12560" />
+                    </td>
+                  </tr>
+                );
+              })}
+            </React.Fragment>
+          )}
         </tbody>
       </table>
     </div>
