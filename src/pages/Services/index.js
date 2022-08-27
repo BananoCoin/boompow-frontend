f455444073e0dd3e36f1cb3e9e59bc32b9446f67
Brandon Berhent <bberhent@ascend-innovations.com>
2022-08-26 22:44:24 -0400

    Some styling on services page

--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -16,26 +16,29 @@ const Services = () => {
 
   return (
     <div className="bg-dark p-2 rounded rounded-l-none w-full max-w-3xl h-5/6 overflow-y-scroll border-banano-yellow border-l-4">
-      <table className="text-gray-300 w-full">
+      <table className="text-gray-300 table-auto w-full">
         <thead>
-          <tr className="[&>*]:font-medium table-fixed [&>*]:py-2 border-b-2 border-banano-yellow/25">
+          <tr className="[&>*]:font-medium table-fixed [&>*]:py-2 border-b-2 border-banano-yellow/25 [&>*]:px-1 [&>*]:sm:px-3 text-left">
             <th>Name</th>
-            <th>Website</th>
-            <th>Requests</th>
+            <th>Total Requests</th>
           </tr>
         </thead>
         <tbody>
           <React.Fragment>
             {stats?.services.map((service) => {
               return (
-                <tr className="[&>*]:first:pt-4 [&>*]:py-1 border-b-1 border-banano-yellow/25 [&>*]:px-1 [&>*]:sm:px-4 [&>*]:md:px-12">
-                  <td>{service.name}</td>
+                <tr className="[&>*]:first:pt-4 [&>*]:py-1 border-b-1 border-banano-yellow/25 [&>*]:px-1 [&>*]:sm:px-3 text-left">
                   <td>
-                    <a target="_blank" rel="noreferrer" href={service.website}>
-                      {service.website}
+                    <a
+                      target="_blank"
+                      rel="noreferrer"
+                      href={service.website}
+                      className="text-banano-yellow hover:text-accent-secondary"
+                    >
+                      {service.name}
                     </a>
                   </td>
-                  <td className="text-right">{service.requests}</td>
+                  <td>{service.requests}</td>
                 </tr>
               );
             })}
