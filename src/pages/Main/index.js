2b81f3a3b0fec1433ee90b3f614f7ff179573a26
Brandon Berhent <bberhent@ascend-innovations.com>
2022-08-30 01:49:23 -0400

    Revert "Attempt to add a footer"
    This reverts commit ca1dc384d7d7bba421c56c98e25d286457ee966c.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -39,7 +39,7 @@ const Main = () => {
   }, [data]);
 
   return (
-    <div className="w-full max-w-5xl h-full flex flex-col items-center text-gray-100 font-semibold text-2xl mt-16">
+    <div className="w-full max-w-5xl h-full flex flex-col items-center text-gray-100 font-semibold text-2xl">
       <div className="mb-6 flex gap-6 flex-col mt-8 md:mt-16 md:flex-row w-full justify-center items-center whitespace-nowrap">
         <TotalPaid amount={stats?.totalPaidBanano} />
         <TotalConnected amount={stats?.totalConnected} />
