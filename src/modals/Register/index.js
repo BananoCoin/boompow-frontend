162e88aba720cb9bbc4cdcc30f22faadf73b8151
Klicer <51763508+Klicer@users.noreply.github.com>
2022-11-08 01:29:50 +0200

    Disable registration, redirect to discord for manual whitelist

--- src/modals/Register/index.js
+++ src/modals/Register/index.js
@@ -1,9 +1,11 @@
-import React from "react";
-import { useSearchParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
+
 import Provider from "./Provider";
+import React from "react";
 import Service from "./Service";
 
-const Register = () => {
+// This is the registration modal that was used before it was switched to manual approval/whitelist only
+const OldRegister = () => {
   let [searchParams, setSearchParams] = useSearchParams();
 
   return (
@@ -35,6 +37,32 @@ const Register = () => {
         </button>
       </div>
       {searchParams.get("type") === "provider" ? <Provider /> : <Service />}
+    </div>
+  );
+};
+
+const Register = () => {
+  let [searchParams, setSearchParams] = useSearchParams();
+
+  return (
+    <div className="flex flex-col p-8 text-gray-300">
+      Registration is set to whitelist only. If you wish to provide or request
+      work, please join our discord and DM bbedward#9246.
+      <div className=" text-gray-200 flex gap-8 items-center mt-8">
+        <div className="text-gray-200 flex flex-col primary">
+          <div>
+            <a
+              href="https://chat.banano.cc"
+              target="_blank"
+              rel="noopener"
+              className="bg-accent hover:bg-accent-secondary text-gray-900 hover:text-gray-800 transition-colors w-min whitespace-nowrap font-medium flex p-2 rounded"
+              title="Join Discord"
+            >
+              Join Discord
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
